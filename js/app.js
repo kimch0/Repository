e172265681e92503769e69a7d923f96bf433ac25
unknown <kimchoxd@gmail.com>
2022-02-12 17:51:36 -0600

    After language change

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function changeLanguage(lang) {
+   
+    if(lang == 'en'){
+       location.href= "/en/index.html";
+    }else if(lang == 'es'){
+        location.href="../index.html"
+    }
+    return false
+}
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
+
